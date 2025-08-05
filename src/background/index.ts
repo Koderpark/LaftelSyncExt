52786d9ef2be6e4323d31bf63b856515ac876723
박성훈 / Koder <koder0205@gmail.com>
2025-08-05 18:09:35 +0900

    feat: alert when room

--- src/background/index.ts
+++ src/background/index.ts
@@ -5,3 +5,18 @@ import * as socket from "./service/socket"
 import * as page from "./service/page"
 
 export { socket, page }
+
+import { Storage } from "@plasmohq/storage"
+
+const storage = new Storage()
+storage.watch({
+  room: (c) => {
+    chrome.action.setBadgeText({
+      text: c.newValue ? "🔴" : ""
+    })
+  }
+})
+
+chrome.action.setBadgeText({
+  text: ""
+})
